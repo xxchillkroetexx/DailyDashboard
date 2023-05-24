c8ada377dfab4005e852f9d25e59b85daf53c2b6
xxchillkroetexx <info@wolf-andreas.eu>
2023-05-24 13:15:27 +0200

    Added SiteHeader Created component folder Updated main.tsx Deleted App.tsx as no longer needed

--- client/src/MainPage.tsx
+++ client/src/MainPage.tsx
@@ -1,17 +1,22 @@
-import React from 'react'; /* Bisher ungenutzter Import, wird später gebraucht. Nicht entfernen*/ 
-import './MainPage.css';
+import React from "react"; /* Bisher ungenutzter Import, wird später gebraucht. Nicht entfernen*/
+import "./MainPage.css";
+import SiteHeader from "./components/SiteHeader";
 
 const MainPage = () => {
   return (
-    /* Body der Hauptseite. Design wird aus MainPage.css gezogen */
-    <div className="MainPage">
-      {/* Die 4 Tiles auf der Seite. Design wird aus MainPage.css gezogen */}
-      <div className="tile"> Hier Tile Contetnt einfügen </div>
-      <div className="tile"> Hier Tile Contetnt einfügen </div>
-      <div className="tile"> Hier Tile Contetnt einfügen </div>
-      <div className="tile"> Hier Tile Contetnt einfügen </div>
-    </div>
+    <>
+      <SiteHeader />
+      {/* Header mit Darkmode, Name und Login */}
+      {/* Body der Hauptseite. Design wird aus MainPage.css gezogen */}
+      <div className="MainPage">
+        {/* Die 4 Tiles auf der Seite. Design wird aus MainPage.css gezogen */}
+        <div className="tile"> Hier Tile Contetnt einfügen </div>
+        <div className="tile"> Hier Tile Contetnt einfügen </div>
+        <div className="tile"> Hier Tile Contetnt einfügen </div>
+        <div className="tile"> Hier Tile Contetnt einfügen </div>
+      </div>
+    </>
   );
-}
+};
 
 export default MainPage;
