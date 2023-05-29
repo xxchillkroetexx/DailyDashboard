edcc848de022d18d3d124ebcf2daeb0ec3cb3196
MrRapGamer <g.julian@gmx.net>
2023-05-29 12:41:22 +0200

    update mit Witzen

---
+++ client/src/components/GetWitz.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+interface IData {
+  // Hier muss die Struktur der Daten definiert werden  
+  type?: string;
+  setup?: string;
+  punchline?: string;
+  id?: number;
+}
+
+function GetWitz() {
+  const [data, setData] = useState<IData | null>(null); // Zustandstyp auf IData oder null setzen
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:50000/joke");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Fehler beim Datenabruf:", error);
+      }
+    };
+    fetchData();
+
+    return;
+  }, []);
+
+
+  return <div>
+    {data?.setup && <p>{setup}</p>}
+    {data?.punchline && <p>{punchline}</p>}
+    </div>;
+}
+
+export default GetWitz;
---
+++ client/src/components/WitzePanel.tsx
@@ -0,0 +1,12 @@
+import GetWitz from "./GetWitz"
+
+function WitzePanel() {
+  return (
+    <div>
+      <h2>Der Witz des Tages</h2>
+        <GetWitz/>
+    </div>
+  );
+}
+
+export default WitzePanel;
