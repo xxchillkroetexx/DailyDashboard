bf77a15e53848e6a496f7218c1d216387c756fa2
xxchillkroetexx <info@wolf-andreas.eu>
2023-05-29 19:28:18 +0200

    added comments

--- client/src/services/auth.service.tsx
+++ client/src/services/auth.service.tsx
@@ -11,24 +11,30 @@ const API_URL = "http://localhost:50000/";
 class AuthService {
   // login of user to
   async login(username: string, password: string) {
+    // fetching credentials from the backend
     const response = await axios.post(API_URL + "login", {
       username: username,
       passwordhash: password,
     });
+    // if the browser has a token, delete it
     if (cookieUtil.getAccessTokenCookie() != null) {
       cookieUtil.deleteAccessTokenCookie();
     }
+    // if the response has a token, set the cookie
     if (response.data.token ? true : false) {
       cookieUtil.setAccessTokenCookie(response.data.token);
     }
     return response.data;
   }
 
+  // logout of user
   logout() {
-    localStorage.removeItem("user");
+    cookieUtil.deleteAccessTokenCookie();
   }
 
+  // register a new user
   register(username: string, email: string, password: string) {
+    // creting a new user with the given credentials and sending it to the backend
     return axios.post(API_URL + "register", {
       username: username,
       email: email,
@@ -37,7 +43,7 @@ class AuthService {
   }
 
   getCurrentUser() {
-    const userStr = localStorage.getItem("user");
+    const userStr = cookieUtil.getAccessTokenCookie();
     if (userStr) return JSON.parse(userStr);
 
     return null;
