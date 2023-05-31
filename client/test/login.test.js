5bbc50ce8181716e0aa8ec1515484e2b08fe6c38
Prommitheus <jonas@perleninsel.de>
2023-05-31 14:13:23 +0200

    Added extensive commentary for all test functions

--- client/test/login.test.js
+++ client/test/login.test.js
@@ -1,26 +1,30 @@
-// Unit test of login.component.tsx
-
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import Login from "../src/components/login.component";
 import { BrowserRouter } from "react-router-dom";
 
-// Testing if Login component renders correctly
+// Unit Test für die Login-Komponente
 
 describe("Login", () => {
   it("should render all login-form components correctly", async () => {
+    // Rendern der Login-Komponente innerhalb des BrowserRouter
     render(
       <BrowserRouter>
         <Login />
       </BrowserRouter>
     );
+
+    // Warten auf das Rendern der Komponente und Überprüfung der Ergebnisse
     await waitFor(() => {
+      // Überprüfen, ob der Text "Login" gerendert wurde
       const loginElement = screen.getByText("Login");
       expect(loginElement).toBeInTheDocument;
 
+      // Überprüfen, ob das Textelement "Username" gerendert wurde
       const usernameElement = screen.getByText("Username");
       expect(usernameElement).toBeInTheDocument;
 
+      // Überprüfen, ob das Textelement "Password" gerendert wurde
       const passwordElement = screen.getByText("Password");
       expect(passwordElement).toBeInTheDocument;
     });
--- client/test/register.test.js
+++ client/test/register.test.js
@@ -1,25 +1,30 @@
-// Unit test of register.component.tsx
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import Register from "../src/components/register.component";
 import { BrowserRouter } from "react-router-dom";
 
-// Testing if Register component renders correctly
+// Unit Test für die Register-Komponente
 
 describe("Register", () => {
   it("should render all register-form components correctly", async () => {
+    // Rendern der Register-Komponente innerhalb des BrowserRouter
     render(
       <BrowserRouter>
         <Register />
       </BrowserRouter>
     );
+
+    // Warten auf das Rendern der Komponente und Überprüfung der Ergebnisse
     await waitFor(() => {
+      // Überprüfen, ob das Textelement "Username" gerendert wurde
       const usernameElement = screen.getByText("Username");
       expect(usernameElement).toBeInTheDocument;
 
+      // Überprüfen, ob das Textelement "Email" gerendert wurde
       const emailElement = screen.getByText("Email");
       expect(emailElement).toBeInTheDocument;
 
+      // Überprüfen, ob das Textelement "Password" gerendert wurde
       const passwordElement = screen.getByText("Password");
       expect(passwordElement).toBeInTheDocument;
     });
