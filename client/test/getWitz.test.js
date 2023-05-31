613f7b11574c92f5d8c4ede7c25f6b753fec8d84
MrRapGamer <g.julian@gmx.net>
2023-05-31 11:23:15 +0200

    update Witze Test

---
+++ client/test/getWitz.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import GetWitz from "../src/components/GetWitz";
+
+
+
+
+describe('GetWitz', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      })
+    );
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should render joke setup and punchline if data is fetched successfully', async () => {
+    const mockData = {
+      setup: 'Why did the chicken cross the road?',
+      punchline: 'To get to the other side!'
+    };
+
+    render(<GetWitz />);
+
+    const setupElement = await screen.findByText(mockData.setup);
+    const punchlineElement = await screen.findByText(mockData.punchline);
+
+    expect(setupElement).toBeInTheDocument();
+    expect(punchlineElement).toBeInTheDocument();
+  });
+
+  it('should handle data fetch error', async () => {
+    const mockError = new Error('Data fetch error');
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+      Promise.reject(mockError)
+    );
+
+    render(<GetWitz />);
+
+    const errorElement = await screen.findByText('Fehler beim Datenabruf:');
+    expect(errorElement).toBeInTheDocument();
+  });
+});
