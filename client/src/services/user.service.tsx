91b30ac0d828d22bfbe2eceac7b11974222d3789
xxchillkroetexx <info@wolf-andreas.eu>
2023-05-31 18:25:54 +0200

    fixed some bugs and added comments

--- client/src/services/user.service.tsx
+++ client/src/services/user.service.tsx
@@ -1,11 +1,11 @@
 /**
- *
- *
+ * This is the user service. It is used to get the public content and the user board.
  */
 
 import axios from "axios";
 import authHeader from "./auth-header";
 
+// set the API_URL
 const API_URL = "http://localhost:50000/";
 
 class UserService {
