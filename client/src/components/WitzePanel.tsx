6a4c9e6a967a1bcdb0629a96c3df80074e9ad7a2
MrRapGamer <g.julian@gmx.net>
2023-06-07 15:05:20 +0200

    Upgrade Witzepanel

--- client/src/components/WitzePanel.tsx
+++ client/src/components/WitzePanel.tsx
@@ -1,10 +1,12 @@
-import GetWitz from "./GetWitz"
+import React, { useState } from "react";
+import GetWitz from "./GetWitz";
 
 function WitzePanel() {
+
   return (
     <div>
       <h2>Der Witz des Tages</h2>
-        <GetWitz/>
+      <GetWitz/>
     </div>
   );
 }
